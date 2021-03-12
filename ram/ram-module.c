869ce75b1ba27381771914c305fd00e4a71cbe5f
Abner-Hernandez <36278298+Abner-Hernandez@users.noreply.github.com>
2021-03-11 22:35:52 -0600

    Update ram-module.c

--- ram/ram-module.c
+++ ram/ram-module.c
@@ -1,9 +1,9 @@
+  GNU nano 4.8                                                                                                                                                  ram-module.c                                                                                                                                                            
 #include <linux/module.h>
 #include <linux/sched.h>
 #include <linux/uaccess.h>
 #include <linux/sysinfo.h>
 #include <linux/slab.h>
-
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -25,9 +25,9 @@
 #include <asm/pgtable.h>
 
 static int my_proc_show(struct seq_file *m, void *v){
-	struct sysinfo i;
-	si_meminfo(&i);
-	seq_printf(m, "%lu",((i.totalram - i.freeram)*100)/i.totalram);
+        struct sysinfo i;
+        si_meminfo(&i);
+        seq_printf(m, "%lu\n%lu\n%lu\n%lu\n",i.totalram, i.totalram - i.freeram, i.freeram, ((i.totalram - i.freeram)*100)/i.totalram);
     return 0;
 }
 
@@ -36,35 +36,33 @@ static ssize_t my_proc_write(struct file* file, const char __user *buffer, size_
 }
 
 static int my_proc_open(struct inode *inode, struct file *file){
-	return single_open(file, my_proc_show, NULL);
+        return single_open(file, my_proc_show, NULL);
 }
 
 static struct file_operations my_fops={
-	.owner = THIS_MODULE,
-	.open = my_proc_open,
-	.release = single_release,
-	.read = seq_read,
-	.llseek = seq_lseek,
-	.write = my_proc_write
+        .owner = THIS_MODULE,
+        .open = my_proc_open,
+        .release = single_release,
+        .read = seq_read,
+        .llseek = seq_lseek,
+        .write = my_proc_write
 };
 
 static int __init initModule(void){
-	struct proc_dir_entry *entry;
-	entry = proc_create("ram-module", 0777, NULL, &my_fops);
-	if(!entry) {
-		return -1;	
-	} else {
-		printk(KERN_INFO "Init_Module_Ram\n");
-	}
-	return 0;
+        struct proc_dir_entry *entry;
+        entry = proc_create("ram-module", 0777, NULL, &my_fops);
+        if(!entry) {
+                return -1;      
+        } else {
+                printk(KERN_INFO "Init_Module_Ram\n");
+        }
+        return 0;
 }
 
 static void __exit exitModule(void){
-	remove_proc_entry("ram-module",NULL);
-	printk(KERN_INFO "End_Module_Ram\n");
+        remove_proc_entry("ram-module",NULL);
+        printk(KERN_INFO "End_Module_Ram\n");
 }
-
-
 
 module_init(initModule);
 module_exit(exitModule);
